85482f264b717144e5c5265e85aced3cbf87365e
oboy21 <oboy11b@gmail.com>
2024-10-03 11:44:21 +0300

    implementing generate method

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fpdf import FPDF
+
 class Bill:
     """
     Object that contains about a bill, such as total amount and period of the bill
@@ -9,7 +11,7 @@ class Bill:
 
 class Flatmate:
     """
-    Creates a flatmate person who lives in the flat and pays ashare of the bill.
+    Creates a flatmate person who lives in the flat and pays a share of the bill.
     """
     def __init__(self, name, days_in_house):
         self.name = name
@@ -19,7 +21,7 @@ class Flatmate:
         to_pay = bill.amount * weight
         return to_pay
 
-    class PdfReport:
+class PdfReport:
         """
         Creates a pdf file that contains data about the flatmates such as their names , their due amount and the period of the bill
 
@@ -28,9 +30,24 @@ class Flatmate:
         def __init__(self, filename):
             self.filename = filename
 
-
         def generate(self, flatmate1, flatmate2, bill):
-            pass
+
+           pdf = FPDF(orientation='P', unit='pt', format='A4')
+           pdf.add_page()
+
+           #Insert title
+           pdf.set_font(family='Times', size=24, style='B')
+           pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+
+           #Insert Period label and value
+           pdf.cell(w=100, h=40, txt="Period:", border=1)
+           pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+           #Insert name and due amount of the first flatmate
+           pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
+           pdf.cell(w=150, h=40, txt=str(flatmate1.pays(bill, flatmate2)), border=1, ln=1)
+
+           pdf.output(self.filename)
+
 
 
 the_bill = Bill(amount=120, period="March 2021")
@@ -39,3 +56,6 @@ marry = Flatmate(name="Marry", days_in_house=25)
 
 print("John pays:", john.pays(bill=the_bill, flatmate2=marry))
 print("Marry pays: ", marry.pays(bill=the_bill, flatmate2=john))
+
+pdf_report = PdfReport(filename="Report1.pdf")
+pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
