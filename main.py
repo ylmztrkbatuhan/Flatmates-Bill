d190cd545bb44a579146a988ffa5403559bc6653
oboy21 <oboy11b@gmail.com>
2024-10-06 12:59:30 +0300

    auto view pdf file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 from fpdf import FPDF
 
 class Bill:
@@ -32,23 +34,36 @@ class PdfReport:
 
         def generate(self, flatmate1, flatmate2, bill):
 
+           flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+           flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
            pdf = FPDF(orientation='P', unit='pt', format='A4')
            pdf.add_page()
 
+           # Add icon
+           pdf.image("house.png", w=30, h=30)
+
+
            #Insert title
            pdf.set_font(family='Times', size=24, style='B')
-           pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align="C", ln=1)
+           pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
 
            #Insert Period label and value
-           pdf.cell(w=100, h=40, txt="Period:", border=1)
-           pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+           pdf.set_font(family="Times", size=14, style='B')
+           pdf.cell(w=100, h=40, txt="Period:", border=0)
+           pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
            #Insert name and due amount of the first flatmate
-           pdf.cell(w=100, h=40, txt=flatmate1.name, border=1)
-           pdf.cell(w=150, h=40, txt=str(flatmate1.pays(bill, flatmate2)), border=1, ln=1)
+           pdf.set_font(family="Times", size=12)
+           pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+           pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
+
+           # Insert name and due amount of the first flatmate
+           pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+           pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
 
            pdf.output(self.filename)
 
-
+           webbrowser.open(self.filename)
 
 the_bill = Bill(amount=120, period="March 2021")
 john = Flatmate(name="john", days_in_house=20)
